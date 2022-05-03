87dd01d8205d619deef3399ffc711a8bafc71f92
Kevin Tellez <kevintellez@Kevin-Tellez-MacBook-Air.local>
2022-05-03 16:55:01 -0500

    Controladores

--- prueba/resources/views/index.blade.php
+++ prueba/resources/views/index.blade.php
@@ -9,6 +9,12 @@
     <p>
         numero: {{ $numero }}
     </p>
+
+<div>
+    <ul>
+        <li><a href="https://www.youtube.com/watch?v=VQPU8l-U1q4">video migracion</a></li>
+    </ul>
+</div>
 </div>
 
 @endsection
